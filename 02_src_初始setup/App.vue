358c41c40271fea3e23f22bd205a9e8305f4601b
KrisShu <1446025061@qq.com>
2022-07-21 17:06:08 +0800

    第一次提交

---
+++ 02_src_初始setup/App.vue
@@ -0,0 +1,43 @@
+<template>
+	<h1>我是app组件</h1>
+	<h2>姓名：{{name}}</h2>
+	<h2>年龄：{{age}}</h2>
+	<button @click="sayHello">点击</button>
+</template>
+
+<script>
+
+export default {
+	name: 'App',
+	setup(){  
+		/* 
+		*注意
+		*1.尽量不要与vue2配置混用
+		*2.setup不能是一个async函数
+		*/
+
+		// 数据
+		let name  = '张三'
+		let age = 18
+
+		// 方法
+		function  sayHello(){
+			console.log(`我叫${name},今年${age}岁`)
+		}
+
+
+		// 返回一个对象（常用）
+		return{
+			name,
+			age,
+			sayHello
+
+		}
+
+	}
+}
+</script>
+
+<style>
+
+</style>
--- 03_src_ref函数/App.vue
+++ 03_src_ref函数/App.vue
@@ -4,14 +4,13 @@
 	<h2>年龄：{{age}}</h2>
 	<h3>工种：{{job.type}}</h3>
 	<h3>薪资：{{job.salary}}</h3>
-	<h3>爱好：{{hobby}}</h3>
 	<button @click="changeInfo">修改信息</button>
 	
 	
 </template>
 
 <script>
-import  {ref,reactive} from 'vue'
+import  {ref} from 'vue'
 export default {
 	name: 'App',
 	setup(){  
@@ -20,35 +19,19 @@ export default {
 		// 数据
 		let name  = ref('张三')
 		let age =  ref(18)
-
-		//ref 响应式处理对象类型的数  求助了reactive函数
-		/* let job = ref({
-			type:'前端',
-			salary:'10k'
-		}) */
-
-		// reactive 处理对象类型的数据
-		let job = reactive({
+		let job = ref({
 			type:'前端',
 			salary:'10k'
 		})
-
-		let hobby = reactive(['看书','逛街'])
-		
 		// 方法
 		function  changeInfo(){
 			// 基本类型数据改变
 			name.value = '李四'
 			age.value = 20
 
-			//ref  对象类型数据的改变
-			/* job.value.type = 'UI'
-			job.value.salary = '20K' */
-
-			// reactive 对象类型数据的改变
-			job.type = 'UI'
-			job.salary = '20K'
-			hobby[0] = '打游戏'
+			// 引用类型数据的改变
+			job.value.type = 'UI'
+			job.value.salary = '20K'
 
 
 		}
@@ -58,7 +41,6 @@ export default {
 			name,
 			age,
 			job,
-			hobby,
 			changeInfo
 		}
 
